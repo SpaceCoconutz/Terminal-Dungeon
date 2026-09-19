889dd89b31362ed2cf5c9689975c33ba15854035
SpaceCoconutz <zayne@promonolo.org>
2026-09-19 10:10:40 -0500

    added welcome to Dungeon

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
@@ -23,6 +24,10 @@ class Player {
             cout << "Attack: " << attack << "\n";
             cout << "Gold: " << gold << "\n";
         }
+
+        bool isAlive() const {
+            return health > 0;
+        }
 };
 
 class Enemy {
@@ -45,25 +50,48 @@ class Enemy {
             cout << "Attack: " << attack << "\n";
             cout << "Gold: " << goldReward << "\n";
         }
+
+        bool isAlive() const {
+            return health > 0;
+        }
 };
 
+void welcome() {
+    cout << "Welcome to my Terminal Dungeon. \n";
+    cout << "There are enemies around every corner. \n";
+    cout << "To begin enter your name. \n";
+    cout << "Once thats complete we will begin! \n";
+}
+
+void enterDungeon(const string& playerName) {
+    cout << "\nWelcome to the dungeon, " << playerName << ".\n";
+    cout << "The doors close behind you...\n";
+    cout << "Good luck.\n";
+}
+
 int main() {
+    system("cls");
+
+    welcome();
+
     string playerName;
 
     cout << "Enter your name: ";
     cin >> playerName;
 
     Player player(playerName);
+    enterDungeon(playerName);
     player.showStats();
 
-    Enemy goblin("Goblin", 50, 10, 25);
-    Enemy skeleton("Skeleton", 70, 15, 40);
-    Enemy dragon("Dragon", 250, 40, 500);
+    // Enemy goblin("Goblin", 50, 10, 25);
+    // Enemy skeleton("Skeleton", 70, 15, 40);
+    // Enemy dragon("Dragon", 250, 40, 500);
 
-    goblin.showStats();
-    skeleton.showStats();
-    dragon.showStats();
+    // goblin.showStats();
+    // skeleton.showStats();
+    // dragon.showStats();
 
+    // cout << "\nAre you alive: " << (player.isAlive() ? "Yes" : "No") << "\n";
 
     return 0;
 }
